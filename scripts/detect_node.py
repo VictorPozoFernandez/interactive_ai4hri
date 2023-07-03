5eee0d94d2681f6515225897e38dc44665345b17
victor <victorpozo125@gmail.com>
2023-07-03 15:13:27 +0200

    model_reasoning_2

--- scripts/detect_node.py
+++ scripts/detect_node.py
@@ -175,7 +175,7 @@ def main():
                     identified_models = {"Output" : elements[0]}
 
                 else:
-                    identified_models = model_reasoning(elements, utterance)
+                    identified_models = model_reasoning_2(elements, utterance)
                     print(identified_models)
 
                 if (identified_models["Output"] == "Lack Information"):
@@ -193,7 +193,7 @@ def main():
                         print("----------------------------------------------")
                         print("Customer: " + answer)
 
-                        identified_models = model_reasoning(elements, "Question: " + question + "Answer: " + answer)
+                        identified_models = model_reasoning_2(elements, "Question: " + question + "Answer: " + answer)
 
             # Send the utterance to the agent
             if (identified_models["Output"] != "Lack Information"):
@@ -283,6 +283,52 @@ def model_reasoning(elements, statement = ""):
     data = extract_json(result.content)
 
     return data
+
+
+def model_reasoning_2(elements, statement = ""):
+
+    # Set OpenAI API credentials
+    openai_api_key = os.environ.get("OPENAI_API_KEY")
+    chat = ChatOpenAI(model_name="gpt-3.5-turbo-0613", temperature=0, openai_api_key=openai_api_key)
+
+    system_template = """
+    You are a helpful assistant that helps to detect which device model and Product_ID is the customer talking about using the List as reference. If two or more models are detected, output "Lack Information". Be concise, don't give explainations.
+    
+    Here there are some examples that illustrates how can you output your answer. The interactions appear in cronological order:
+
+    Customer: How much does this Sony Alpha camera cost?
+    List: [(1, 'Nikon Coolpix S2800'), (2, 'Sony Alpha a6000'), (3, 'Canon EOS 5D Mark III'), (4, 'Xiaomi T11 Pro'), (5, 'Huawei airpods 4j'), (6, 'Sony Alpha a5000'), (7, 'Xiaomi Mi A3')]
+    You: {"Reasoning": "The Customer is talking about a Sony Alpha model. There are two possible models that fit this criteria ((2, 'Sony Alpha a6000'), (6, 'Sony Alpha a5000'))", "Detection" : "(2, 'Sony Alpha a6000'), (6, 'Sony Alpha a5000')", "Output": "Lack Information"}
+
+    Customer: How much does this Nikon Coolpix camera cost?
+    List: [(1, 'Nikon Coolpix S2800'), (2, 'Sony Alpha a6000'), (3, 'Canon EOS 5D Mark III'), (4, 'Xiaomi T11 Pro'), (5, 'Huawei airpods 4j'), (6, 'Sony Alpha a5000'), (7, 'Xiaomi Mi A3')]
+    You: {"Reasoning": "The Customer is talking about a Sony Alpha model. There is only one model that fit this criteria ((1, 'Nikon Coolpix S2800'))", "Detection" : "(1, 'Nikon Coolpix S2800')", "Output": "(1, 'Nikon Coolpix S2800')"}
+
+    Customer: How much does this device cost?
+    List: [(1, 'Nikon Coolpix S2800'), (2, 'Sony Alpha a6000'), (3, 'Canon EOS 5D Mark III')]
+    You: {"Reasoning": "The Customer is not explicitely mentioning any model. No model is detected", "Detection" : "(1, 'Nikon Coolpix S2800'), (2, 'Sony Alpha a6000'), (3, 'Canon EOS 5D Mark III')", "Output": "Lack Information"}
+    
+    Output the answer only in JSON format.
+    """
+
+    user_template = """
+    Customer: {statement}
+    List: {elements}
+    """
+
+    user_prompt_template = PromptTemplate(input_variables=["elements", "statement"], template=user_template)
+    user_prompt = user_prompt_template.format( elements = elements, statement = statement)
+
+    prompt_history = [
+        SystemMessage(content=system_template),
+        HumanMessage(content=user_prompt)
+    ]
+
+    result = chat(prompt_history)
+    data = extract_json(result.content)
+
+    return data
+
  
 def question_reasoning(identified_models):
     
